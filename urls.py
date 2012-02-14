12c637488447523f205023fd525bff2a5d90c6e0
Luke Sneeringer <lukesneeringer@gmail.com>
2012-02-14 16:56:19 -0600

    Updating urls.py

--- urls.py
+++ urls.py
@@ -5,13 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sneeringer.views.home', name='home'),
-    # url(r'^sneeringer/', include('sneeringer.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # django admin
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
