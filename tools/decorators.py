c2cac994bd36a3791e3853cd32915ad4072da534
Luke Sneeringer <lukesneeringer@gmail.com>
2012-02-21 13:27:20 -0600

    Complete invitation models.

---
+++ tools/decorators.py
@@ -0,0 +1,24 @@
+def cached_property(method):
+    """Decorator that turns a method into a special kind of property;
+    it retrieves the data requested by the method, and caches the result
+    so that future calls to the property do not invoke the computation time.
+    
+    This decorator is intended for expensive property calls (e.g. how many threads in this forum?)
+    which, while they may change often, are unlikely to change during the life cycle of a request."""
+    
+    def f(self):
+        # do I have a metadata repository?
+        if not hasattr(self, '_metadata_cache'):
+            self._metadata_cache = {}
+
+        # do I already have the result cached?
+        if method.__name__ in self._metadata_cache:
+            return self._metadata_cache[method.__name__]
+            
+        # okay, I don't so I need to run the method
+        answer = method(self)
+        
+        # cache the response
+        self._metadata_cache[method.__name__] = answer
+        return self._metadata_cache[method.__name__]
+    return property(f)
--- wedding/rsvp/models.py
+++ wedding/rsvp/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from tools.decorators import cached_property
 
 
 class Invitation(models.Model):
@@ -10,20 +12,41 @@ class Invitation(models.Model):
     state = models.CharField(max_length=2)
     zip_code = models.CharField(max_length=5)
     rehearsal_dinner = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
     
-    @property
-    def formal_name(self):
+    def __unicode__(self):
         """Return the formal name used to address this invitation."""
+
+        # if there is only one adult, use the adult's formal name
+        if len(self.adults) == 1:
+            return unicode(self.adults[0])
+            
+        # okay, there are two adults -- if their last names match,
+        # then we want a single, unified format
+        # (e.g. "Mr. & Mrs. Jonathan Chappell")
+        if self.adults[0].last_name == self.adults[1].last_name:
+            return u'%(male_title)s & %(female_title)s %(male_first_name)s %(last_name)s' % {
+                'female_title': self.adults[1].title,
+                'last_name': self.adults[0].last_name,
+                'male_title': self.adults[0].title,
+                'male_first_name': self.adults[0].first_name,
+            }
+            
+        # okay, the last names don't match; print the names out separately
+        # and in their entirety
+        # (e.g. "Mr. Daniel Beltz and Ms. Jessica Dymer")
+        return u'%s & %s' % (self.adults[0], self.adults[1])
     
-    @property
+    @cached_property
     def adults(self):
-        return self.invitee_set.filter(age_group='adult')
+        return self.invitee_set.filter(age_group='adult').order_by('-sex')
         
-    @property
+    @cached_property
     def children(self):
         return self.invitee_set.filter(age_group='children')
         
-    @property
+    @cached_property
     def infants(self):
         return self.invitee_set.filter(age_group='infants')
         
@@ -38,9 +61,16 @@ class Invitee(models.Model):
         ('adult', 'Adult'),
         ('child', 'Child (2 - 16 years)'),
         ('infant', 'Infant (0 - 23 months)'),
-    ), default='adult')
+    ), default='adult', db_index=True)
+    sex = models.CharField(max_length=6, choices=(
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ), db_index=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return '%s %s %s' % (self.title, self.first_name, self.last_name)
     
 
 class RSVP(models.Model):
@@ -53,8 +83,29 @@ class RSVP(models.Model):
         ('chicken', 'Chicken'),
         ('fish', 'Fish'),
         ('steak', 'Steak'),
-        ('vegetarian', 'Vegetarian')
+        ('vegetarian', 'Vegetarian'),
         ('gluten-free', 'Gluten Free'),
     ), null=True, blank=True)
+    medium = models.CharField(max_length=10, choices=(
+        ('mail', 'Mail'),
+        ('online', 'Online'),
+    ), help_text='The response medium this person used.', default='mail')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return u'RSVP: %s' % self.invitee
+    
+
+class Gift(models.Model):
+    """A mapping between an invitation and the gift we received from
+    the invitees. For thank you notes."""
+    
+    invitation = models.ForeignKey(Invitation)
+    label = models.CharField(max_length=250, help_text='A terse statement of "what this gift is". For instance, "linen sheets", "gaming table".')
+    notes = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return self.label
